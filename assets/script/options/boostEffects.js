934cf5056e50288cb17af289719705943c82dfb1
Leo Delplanque <delplanqueleomail@gmail.com>
2022-05-25 01:20:07 +0200

    :lipstick: change assets :)

--- assets/script/options/boostEffects.js
+++ assets/script/options/boostEffects.js
@@ -1,22 +1,24 @@
 export const boostEffect = [
     {
-        "name" : "ammunition",
-        "effect": function(){ this.player.addAmunition(20) },
-        "src": './assets/images/bullet_icons.jpg'
-    }, 
-    {
-        "name" : "rapidfire",
-        "effect": function(){
-            this.player.rapidFire =true
-            this.player.rapidFireFunction() 
+        name: 'ammunition',
+        effect: function () {
+            this.player.addAmunition(20)
         },
-        "src": './assets/images/rapidfire_icon.jpg'
+        src: './assets/images/bullet_icons.png',
     },
     {
-        "name" : "shield",
-        "effect": function(){
+        name: 'rapidfire',
+        effect: function () {
+            this.player.rapidFire = true
+            this.player.rapidFireFunction()
+        },
+        src: './assets/images/rapidfire_icon.png',
+    },
+    {
+        name: 'shield',
+        effect: function () {
             this.player.shield = true
         },
-        "src": './assets/images/shield.png'
+        src: './assets/images/shield.png',
     },
 ]
